7c22beebba71df786244675b72d10eed93cbfc3e
Mads Harby <71691732+madshby@users.noreply.github.com>
2021-03-02 11:39:45 +0100

    Added GUI
    skrt

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,11 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +15,27 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    public ImageView rockImage;
+    public Button rockButton;
+    public ImageView paperImage;
+    public Button paperButton;
+    public ImageView scissorsImage;
+    public Button scissorsButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    public void handleRockButton(ActionEvent actionEvent) {
+    }
+
+    public void handlePaperButton(ActionEvent actionEvent) {
+    }
+
+    public void handleScissorsButton(ActionEvent actionEvent) {
     }
 }
